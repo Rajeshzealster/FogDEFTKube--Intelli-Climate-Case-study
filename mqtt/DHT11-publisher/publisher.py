0e05dbe8740fd166e2365eb047268fdf9df8f543
Rajesh Thalla <97143348+Rajeshzealster@users.noreply.github.com>
2023-12-13 11:54:56 +0530

    Update publisher.py

--- mqtt/DHT11-publisher/publisher.py
+++ mqtt/DHT11-publisher/publisher.py
@@ -1,9 +1,18 @@
+import sys
 import RPi.GPIO as GPIO
 import dht11
 import time
 import datetime
 import json
 import paho.mqtt.client as mqtt
+
+if len(sys.argv) == 2:
+    user=sys.argv[1]
+else:
+    # If the AUTHOR argument is not provided, display information on how to execute the program
+    print("Usage: python your_script.py AUTHOR")
+    print("Example: python your_script.py Thalla")
+    sys.exit(1)  # Exit with a non-zero status code to indicate an error
 
 # initialize GPIO
 GPIO.setwarnings(True)
@@ -15,7 +24,7 @@ instance = dht11.DHT11(pin=14)
 # MQTT broker settings
 broker_address = "192.168.0.204"
 broker_port = 30001
-topic = "sensor/data"
+topic = "sensor/"+user+"/data"
 
 # MQTT client initialization
 client = mqtt.Client()
@@ -33,7 +42,7 @@ try:
             # publish sensor data to MQTT broker
             payload = json.dumps({"temperature": result.temperature, "humidity": result.humidity})
             client.publish(topic, payload)
-            print("Published to MQTT broker")
+            print("Published to MQTT broker from "+user)
 
         time.sleep(6)
 
